756b90fc7e5bc571b38179c0b2e2916195cb3de6
Abdallah Yasser <abdallahyasser1277@gmail.com>
2023-06-07 23:24:34 +0200

    add Roll Button and add photos to resources

--- app/src/main/java/com/abdalllahyascer/diceroller/MainActivity.kt
+++ app/src/main/java/com/abdalllahyascer/diceroller/MainActivity.kt
@@ -2,10 +2,22 @@ package com.abdalllahyascer.diceroller
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val rollButton: Button=findViewById(R.id.roll_btn)
+        rollButton.text ="Let's Roll"
+        rollButton.setOnClickListener {
+            rollDice()
+        }
+    }
+
+    private fun rollDice() {
+        val randomInt = Random().nextInt(6) + 1
     }
 }
